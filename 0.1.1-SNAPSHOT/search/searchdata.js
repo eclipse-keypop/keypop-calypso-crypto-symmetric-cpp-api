4a9c20c58930e19dfce905d34d2a99fceccc52f5
jeanpierrefortune <jeanpierrefortune@users.noreply.github.com>
2024-08-27 14:18:41 +0000

    deploy: 416642f03e08f7048fc61bebe66ffbf9bea3f637

---
+++ 0.1.1-SNAPSHOT/search/searchdata.js
@@ -0,0 +1,27 @@
+var indexSectionsWithContent =
+{
+  0: "acdfgikpsu",
+  1: "s",
+  2: "k",
+  3: "s",
+  4: "acdfgipsu"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "namespaces",
+  3: "files",
+  4: "functions"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Namespaces",
+  3: "Files",
+  4: "Functions"
+};
+
