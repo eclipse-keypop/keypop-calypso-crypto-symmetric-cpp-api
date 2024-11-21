b315ec047c12cb4fce45128dcacfcc7080bde431
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2024-11-21 15:28:25 +0100

    docs: remove obsolete RC docs

--- 0.1.1-rc1-SNAPSHOT/search/all_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['finalizeterminalsessionmac_0',['finalizeTerminalSessionMac',['../classkeypop_1_1calypso_1_1crypto_1_1symmetric_1_1spi_1_1_symmetric_crypto_card_transaction_manager_spi.html#a9f1666e6162f27d89882624220ee5b21',1,'keypop::calypso::crypto::symmetric::spi::SymmetricCryptoCardTransactionManagerSpi']]]
-];
--- 0.1.1-rc1-SNAPSHOT/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['keypop_20calypso_20crypto_20symmetric_20c_2b_2b_20api_20documentation_0',['Keypop Calypso Crypto Symmetric C++ API Documentation',['../index.html',1,'']]]
-];
